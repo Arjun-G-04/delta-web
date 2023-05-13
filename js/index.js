56fa67c6ce6462de4be187a3ab095520f64be24f
Arjun <34741958+Arjun-G-04@users.noreply.github.com>
2023-05-14 01:06:05 +0530

    started working on food seq

--- js/index.js
+++ js/index.js
@@ -5,8 +5,10 @@ let score = 0 ;
 let lastPaintTime = 0 ;
 let snake = [{x: 13, y: 15}, {x:14, y:15}, {x:15, y:15}] ;
 let food = {x: 5, y: 10} ;
+let hiScore = 0 ;
+let foods = genFood() ;
 
-// Game sequence (regulate fram rate)
+// Game sequence (regulate frame rate)
 function main(ctime) {
     window.requestAnimationFrame(main);
     if (((ctime - lastPaintTime)/1000) < (1/speed)) {
@@ -17,7 +19,6 @@ function main(ctime) {
 }
 
 // Game functions
-
 function isCollide(snakeArr) {
 
     // If collide with itself
@@ -28,14 +29,40 @@ function isCollide(snakeArr) {
     }
 
     // If moves past walls
-    if (snake[0].x < 1 || snake[0].x > 18) {
+    if (snake[0].x < 1 || snake[0].x > 20) {
         return true ;
     }
-    if (snake[0].y < 1 || snake[0].y > 18) {
+    if (snake[0].y < 1 || snake[0].y > 20) {
         return true ;
     }
 
     return false ;
+}
+
+function genFood() {
+    genFoods = [] ;
+    colours = ["red", "yellow", "green", "blue", "cyan"] ;
+    let a = 1 ;
+    let b = 20 ;
+    let i = 0 ;
+    while (true) {
+        loc = {x: Math.round(a+(b-a)*Math.random()), y: Math.round(a+(b-a)*Math.random())} ;
+        if (!genFoods.includes(loc) && !snake.includes(loc)) {
+            loc['colour'] = colours[i] ;
+            genFoods.push(loc) ;
+            i++ ;
+        }
+        if (i === 5) {
+            break ;
+        }
+    }
+    return genFoods ;
+}
+
+function randomOrder() {
+    let normOrder = [0, 1, 2, 3, 4] ;
+    randOrder = normOrder.sort(() => Math.random() - 0.5) ;
+    return randOrder ;
 }
 
 // Game Engine: Renders after taking input and evaluating under game rules
@@ -47,15 +74,19 @@ function gameEngine() {
         direction = {x: 0, y: 0} ;
         snake = [{x: 13, y: 15}, {x:14, y:15}, {x:15, y:15}] ;
         score = 0 ;
+        scoreBox.innerHTML="Score: " + score ;
     }
 
     // If ate food, increase score, regenerate food
-
     if (food.x === snake[0].x && food.y === snake[0].y) {
         let a = 1 ;
         let b = 18 ;
         food = {x: Math.round(a+(b-a)*Math.random()), y: Math.round(a+(b-a)*Math.random())} ;
         score++ ;
+        if (score > hiScore) {
+            localStorage.setItem("hiScore", JSON.stringify(score)) ;
+        }
+        scoreBox.innerHTML="Score: " + score ;
     }
 
     // Move snake if input started
@@ -81,11 +112,26 @@ function gameEngine() {
     });
 
     // Display food
-    foodElement = document.createElement('div') ;
-    foodElement.style.gridRowStart = food.y ;
-    foodElement.style.gridColumnStart = food.x ;
-    foodElement.classList.add('food') ;
-    board.appendChild(foodElement) ;
+
+    for (let i = 0 ; i < foods.length ; i++) {
+        food = foods[i] ;
+        foodElement = document.createElement('div') ;
+        foodElement.classList.add('food') ;
+        foodElement.style.backgroundColor = food["colour"] ;
+        foodElement.style.gridRowStart = food.y ;
+        foodElement.style.gridColumnStart = food.x ;
+        board.appendChild(foodElement) ;
+    }
+
+    // Update High Score
+    let hiScoreData = localStorage.getItem("hiScore") ;
+    if (hiScoreData === null) {
+        localStorage.setItem("hiScore", JSON.stringify(hiScore)) ;
+    }
+    else {
+        hiScore = JSON.parse(hiScoreData) ;
+        highScore.innerHTML = "HiScore: " + hiScore ;
+    }
 }
 
 // Process the input
